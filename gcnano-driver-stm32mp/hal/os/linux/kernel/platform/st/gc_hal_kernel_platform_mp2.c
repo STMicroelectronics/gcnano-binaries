21a47342d500b06ebce617ee7530318bdf642b33
Nicolas LOUBOUTIN <nicolas.louboutin@st.com>
2025-11-27 09:49:31 +0100

    GCnano 6.4.21 release for Android
    Signed-off-by: Nicolas LOUBOUTIN <nicolas.louboutin@st.com>

--- gcnano-driver-stm32mp/hal/os/linux/kernel/platform/st/gc_hal_kernel_platform_mp2.c
+++ gcnano-driver-stm32mp/hal/os/linux/kernel/platform/st/gc_hal_kernel_platform_mp2.c
@@ -591,7 +591,7 @@ _GetPower(IN gcsPLATFORM * Platform)
 #endif
     }
 
-    priv->clk_3d_axi = devm_clk_get(dev, "bus");
+    priv->clk_3d_axi = devm_clk_get(dev, "axi");
     if (IS_ERR(priv->clk_3d_axi)) {
         if (PTR_ERR(priv->clk_3d_axi) != -EPROBE_DEFER)
            dev_err(dev, "no AXI clock");
